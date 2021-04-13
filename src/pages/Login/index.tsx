2d7dd876e8fe55d242f4a6e8feea3e843f623b88
chenghaopeng <i@chper.cn>
2021-04-13 21:22:18 +0800

    注册

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -12,18 +12,39 @@ import store from '../../utils/store'
 export interface LoginProps extends RouterProps {}
 
 function Login (props: LoginProps) {
+  const [login, setLogin] = useState(true)
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
+  const [repeat, setRepeat] = useState('')
   const handleLogin = () => {
-    Api.user.login({ username, password }).then(res => {
-      if (res.success) {
-        store.set('user', res.content)
-        alert(res.content.username + '，你好呀～！')
-        props.history.push(routes[1].path)
-      } else {
-        alert('登录失败！' + res.content)
+    if (login) {
+      Api.user.login({ username, password }).then(res => {
+        if (res.success) {
+          store.set('user', res.content)
+          alert(res.content.username + '，你好呀～！')
+          props.history.push(routes[1].path)
+        } else {
+          alert('登录失败！' + res.content)
+        }
+      })
+    } else {
+      if (password !== repeat) {
+        alert('两次密码输入要一致！')
+        return
       }
-    })
+      Api.user.register({ username, password }).then(res => {
+        if (res.success) {
+          alert('注册成功！')
+          setLogin(true)
+          setRepeat('')
+        } else {
+          alert('注册失败！' + res.content)
+        }
+      })
+    }
+  }
+  const loginText = (flag: boolean) => {
+    return flag === login ? '登 录' : '注 册'
   }
   return (
     <div className={styles.whole}>
@@ -31,10 +52,14 @@ function Login (props: LoginProps) {
         <FontAwesomeIcon icon={faUser} color="#69BBFF" size="5x" />
       </div>
       <div className={styles.input}>
-        <MyInput placeholder="账号" onChange={setUsername} />
-        <MyInput placeholder="密码" password onChange={setPassword} />
+        <MyInput placeholder="账号" value={username} onChange={setUsername} />
+        <MyInput placeholder="密码" password value={password} onChange={setPassword} />
+        {login || <MyInput placeholder="重复密码" password value={repeat} onChange={setRepeat} />}
       </div>
-      <MyButton primary title="登 录" onClick={handleLogin} />
+      <div className={styles.control}>
+        <MyButton primary title={loginText(true)} onClick={handleLogin} />
+        <MyButton title={loginText(false)} onClick={() => setLogin(!login)} />
+      </div>
     </div>
   )
 }
