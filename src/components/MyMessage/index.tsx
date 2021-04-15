3da830825a755a67bc91bde713faa61169d5bb4e
chenghaopeng <i@chper.cn>
2021-04-16 00:00:22 +0800

    初步完成消息组件

---
+++ src/components/MyMessage/index.tsx
@@ -0,0 +1,55 @@
+import React, { forwardRef, useImperativeHandle, useState } from "react"
+import ReactDOM from "react-dom"
+import styles from './index.module.scss'
+
+export type Message = {
+  id: number;
+  text: string;
+}
+
+export type MessageContainerRef = {
+  add: (text: string, duration: number) => void;
+}
+
+const MessageContainer = forwardRef((_: {}, ref: React.Ref<MessageContainerRef>) => {
+  const [messages, setMessages] = useState<Array<Message>>([])
+  useImperativeHandle(ref, () => ({
+    add: (text: string, duration: number) => {
+      const id = Math.random()
+      setMessages(messages => [...messages, { id, text }])
+      setTimeout(() => {
+        setMessages(messages => messages.filter(message => message.id !== id))
+      }, duration)
+    }
+  }))
+  return (
+    <>
+      {messages.map(message => (
+        <div key={message.id} className={styles.message}>{ message.text }</div>
+      ))}
+    </>
+  )
+})
+
+const ref = React.createRef<MessageContainerRef>()
+
+function install () {
+  const messageContainer = document.createElement('div')
+  messageContainer.classList.add(styles.whole)
+  document.body.append(messageContainer)
+  ReactDOM.render(<MessageContainer ref={ref} />, messageContainer)
+}
+
+let _message = (text: string, duration: number) => {
+  install()
+  _message = (text: string, duration: number) => {
+    ref.current?.add(text, duration)
+  }
+  _message(text, duration)
+}
+
+function message (text: string, duration = 3000) {
+  _message(text, duration)
+}
+
+export default message
